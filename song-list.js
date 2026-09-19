681172e65133b63df0bd2f8342d36516f34f2953
IggigSalad <80287019+IggigSalad@users.noreply.github.com>
2026-09-18 23:13:49 -0400

    I did it myself this time

---
+++ song-list.js
@@ -0,0 +1,47 @@
+var song_list = [
+   {
+      title: "File Select",
+      album: "Kirby 64: The Crystal Shards",
+      source: "https://lambda.vgmtreasurechest.com/soundtracks/kirby-the-crystal-shards/juvxdcsrrb/1-02.%20File%20Select.mp3",
+   },
+   {
+      title: "King Dedede's Castle",
+      album: "Kirby 64: The Crystal Shards",
+      source: "King Dedede's Castle.mp3",
+   },
+   {
+      title: "On The Beach",
+      album: "Yoshi's Story",
+      source: "https://lambda.vgmtreasurechest.com/soundtracks/yoshi-s-story-nintendo-64-gamerip/hylelgioau/05.%20On%20The%20Beach.mp3",
+   },
+   {
+      title: "Come Back To Me",
+      album: "Yoshi's Story",
+      source: "https://lambda.vgmtreasurechest.com/soundtracks/yoshi-s-story-nintendo-64-gamerip/mpznddqujp/17.%20Come%20Back%20To%20Me.mp3",
+   },
+   {
+      title: "Rock Star",
+      album: "Kirby 64: The Crystal Shards",
+      source: "https://lambda.vgmtreasurechest.com/soundtracks/kirby-the-crystal-shards/isteyjcmma/1-22.%20Rock%20Star.mp3",
+   },
+   {
+      title: "Ruins",
+      album: "Kirby 64: The Crystal Shards",
+      source: "https://lambda.vgmtreasurechest.com/soundtracks/kirby-the-crystal-shards/twmqtorusz/1-23.%20Ruins.mp3",
+   },
+   {
+      title: "Overworld of Hyrule",
+      album: "The Legend of Zelda",
+      source: "https://lambda.vgmtreasurechest.com/soundtracks/the-legend-of-zelda-nes/xrrhlksi/02.%20Overworld%20of%20Hyrule.mp3",
+   },
+   {
+      title: "Mondo Greens",
+      album: "[REMIX] Kirby's Dream Land",
+      source: "Mondo Greens.wav",
+   },
+   {
+      title: "To Be Continued",
+      album: "Poke&#769;mon",
+      source: "To Be Continued.mp3",
+   },
+];
